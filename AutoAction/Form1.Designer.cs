44b15f7121e45c2ac7bcefb016369df192ec2fb3
Nguyen Tuan Kiet <nguyentuankiet23031998@gmail.com>
2017-12-05 13:28:36 +0700

    Add project files.

---
+++ AutoAction/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace AutoAction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbX = new System.Windows.Forms.TextBox();
+            this.btClick = new System.Windows.Forms.Button();
+            this.tbY = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Position:";
+            // 
+            // tbX
+            // 
+            this.tbX.Location = new System.Drawing.Point(12, 37);
+            this.tbX.Name = "tbX";
+            this.tbX.Size = new System.Drawing.Size(75, 20);
+            this.tbX.TabIndex = 1;
+            // 
+            // btClick
+            // 
+            this.btClick.Location = new System.Drawing.Point(12, 63);
+            this.btClick.Name = "btClick";
+            this.btClick.Size = new System.Drawing.Size(156, 23);
+            this.btClick.TabIndex = 2;
+            this.btClick.Text = "Auto Click";
+            this.btClick.UseVisualStyleBackColor = true;
+            this.btClick.Click += new System.EventHandler(this.btClick_Click);
+            // 
+            // tbY
+            // 
+            this.tbY.Location = new System.Drawing.Point(93, 37);
+            this.tbY.Name = "tbY";
+            this.tbY.Size = new System.Drawing.Size(75, 20);
+            this.tbY.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(182, 104);
+            this.Controls.Add(this.tbY);
+            this.Controls.Add(this.btClick);
+            this.Controls.Add(this.tbX);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbX;
+        private System.Windows.Forms.Button btClick;
+        private System.Windows.Forms.TextBox tbY;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ AutoAction/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoAction
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //This is a replacement for Cursor.Position in WinForms
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        //This simulates a left mouse click
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+
+        private void btClick_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(2000);
+            LeftMouseClick(int.Parse(tbX.Text), int.Parse(tbY.Text));
+            SendKeys.SendWait("1000");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = "Position: " + Cursor.Position.X + " , " + Cursor.Position.Y;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
